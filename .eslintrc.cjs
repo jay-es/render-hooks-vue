b0ad0f69c3335ace356178ca15b550b4e4bb43d9
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-07-29 12:10:30 +0900

    settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
